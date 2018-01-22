f6674f19f6404280cb3c78df90aa6187f79dfac3
KIMMINJAE <innersme@gmail.com>
2018-01-22 20:25:45 +0900

    Ch4

---
+++ Ch04_Q3_2.cpp
@@ -0,0 +1,33 @@
+//
+//  Ch04_Q3_2.cpp
+//  Cpp
+//
+//  Created by MJMacBook on 2018. 1. 18..
+//  Copyright © 2018년 MJMacBook. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace COMP_POS
+{
+    enum
+    {
+        CLERK = 0,
+        SENIOR ,
+        ASSIST ,
+        MANAGER
+    };
+}
+
+class NameCard
+{
+    char *Name;
+    char *Company;
+    char *PNum;
+    int
+public:
+    NameCard(stirng Name, string Company, string PNum,  );
+    
+};
---
+++ fruitSaleSim3.cpp
@@ -0,0 +1,9 @@
+//
+//  fruitSaleSim3.cpp
+//  Cpp
+//
+//  Created by MJMacBook on 2018. 1. 18..
+//  Copyright © 2018년 MJMacBook. All rights reserved.
+//
+
+#include <stdio.h>
