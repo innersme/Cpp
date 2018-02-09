13b1d9e4a1bdbaddd9c38dab5e5444d88ca51334
KIMMINJAE <innersme@gmail.com>
2018-02-09 15:50:34 +0900

    OOPProject01

---
+++ BankingSystemVer01.cpp
@@ -0,0 +1,32 @@
+//
+//  BankingSystemVer01.cpp
+//  Cpp
+//
+//  Created by MJMacBook on 2018. 2. 9..
+//  Copyright © 2018년 MJMacBook. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+enum {MAKE = 1, DEPOSIT, WITHDRAW, PRINT, EXIT};
+
+class
+
+void Menu()
+{
+    cout <<" ----Menu-----" << endl;
+    cout << "1. 계좌개설" << endl;
+    cout << "2. 입 금" << endl;
+    cout << "3. 출 금" << endl;
+    cout << "4. 계좌정보 전체 출력" <<endl;
+    cout << "5. 프로그램 종료" << endl;
+}
+
+int main()
+{
+    int select;
+    Menu();
+    
+    return 0;
+}
