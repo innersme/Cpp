891a54d9a6937a36e86e61bd0ef5cbf12b0164f2
KIMMINJAE <innersme@gmail.com>
2018-02-11 23:47:25 +0900

    OOPProject02 MakeFunction

---
+++ BankingSystemVer02.cpp
@@ -0,0 +1,39 @@
+//
+//  BankingSystemVer02.cpp
+//  Cpp
+//
+//  Created by MJMacBook on 2018. 2. 11..
+//  Copyright © 2018년 MJMacBook. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// enum
+enum {MAKE = 1, DEPOSIT, WITHDRAW, PRINT, EXIT};
+
+// Account class
+class Account
+{
+    int AccountN;
+    
+    
+public:
+    
+};
+
+void Menu()
+{
+    cout <<" ----Menu-----" << endl;
+    cout << "1. 계좌개설" << endl;
+    cout << "2. 입 금" << endl;
+    cout << "3. 출 금" << endl;
+    cout << "4. 계좌정보 전체 출력" <<endl;
+    cout << "5. 프로그램 종료" << endl;
+}
+
+int main()
+{
+    
+}
